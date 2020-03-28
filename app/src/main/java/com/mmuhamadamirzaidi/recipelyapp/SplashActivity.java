9ed1be281b14118f5161f592ea426ae0659ad4c5
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2020-03-28 23:55:39 +0800

    Add Splash Screen

---
+++ app/src/main/java/com/mmuhamadamirzaidi/recipelyapp/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.mmuhamadamirzaidi.recipelyapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView SplashLogo;
+    private int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Hide the Status Bar and the Navigation Bar
+
+        View overlay = findViewById(R.id.splashlogo);
+
+        overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        SplashLogo = findViewById(R.id.splash_logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.splash);
+        SplashLogo.startAnimation(myanim);
+    }
+}
